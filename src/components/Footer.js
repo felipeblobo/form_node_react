207ab22346eb6fce0adca03bd7fd47283c08f873
Felipe Lobo <felipeblobo@gmail.com>
2021-04-11 18:48:48 -0300

    adequando cores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Footer() {
   return (
     <footer>
-      <p className='footerItem'>Alguns direitos reservados.</p>
-      <div className='footerItem'>
+      <p className='footerItem footerMargin'>Alguns direitos reservados.</p>
+      <div className='footerItem footerMargin'>
         <a target='_blank' href='https://github.com/felipeblobo' alt='github'><FontAwesomeIcon className = 'icone' icon={faGithub} size="2x" style={{color: '#588e55'}} /></a>
         <a target='_blank' href='https://www.linkedin.com/in/felipelobo23/' alt='linkedin'><FontAwesomeIcon icon={faLinkedin} size="2x" style={{color: '#588e55'}} /></a>
       </div>
-      <p className='footerItem'>Salvador, Bahia.</p>
+      <p className='footerItem '>Salvador, Bahia.</p>
     </footer>
   );
 }
